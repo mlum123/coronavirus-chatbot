da6c7e87677150bc2c7d4a381ecd84512710f0e9
mlum123 <michellelum123@gmail.com>
2021-01-17 15:42:26 -0800

    styled ChatLoading dots

--- src/components/ChatLoading.js
+++ src/components/ChatLoading.js
@@ -1,16 +1,24 @@
 // Chat Loading Dots component to display when waiting for chatbot response
 import React from "react";
 import "./ChatLoading.css";
+import { Row, Col } from "reactstrap";
 
 class ChatLoading extends React.Component {
   render() {
     return (
       <>
-        <div class="spinner">
-          <div class="bounce1"></div>
-          <div class="bounce2"></div>
-          <div class="bounce3"></div>
-        </div>
+        <Row>
+          <Col xs="2">
+            <i className="fab fa-android fa-3x bot-chat-icon"></i>
+          </Col>
+          <Col xs="10">
+            <div class="spinner">
+              <div class="bounce1"></div>
+              <div class="bounce2"></div>
+              <div class="bounce3"></div>
+            </div>
+          </Col>
+        </Row>
       </>
     );
   }
